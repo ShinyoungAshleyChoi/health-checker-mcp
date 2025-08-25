aaef049982b4494bb3e13898fab2b68d88215d19
shinyoungashleychoi <choishin0@gmail.com>
2025-08-25 17:26:43 +0900

    Initial Commit

---
+++ ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by shinyoung choi on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
